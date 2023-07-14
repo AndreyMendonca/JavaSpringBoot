d53f92444e95a2f2487aea8923e161b54bbb9bec
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-07-14 14:56:10 -0300

    Projeto Spring Boot criado

---
+++ webServiceSpringBootJPA/src/main/java/com/andrey/webServiceSpringBootJPA/WebServiceSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.webServiceSpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ webServiceSpringBootJPA/src/test/java/com/andrey/webServiceSpringBootJPA/WebServiceSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.webServiceSpringBootJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
