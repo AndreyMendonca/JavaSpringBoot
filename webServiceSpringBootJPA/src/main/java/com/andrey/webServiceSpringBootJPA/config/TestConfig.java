45537ceb826adc81f77374b8fef65d1eb41db3b1
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-07-20 11:39:59 -0300

    Associação muitos para muitos

--- webServiceSpringBootJPA/src/main/java/com/andrey/webServiceSpringBootJPA/config/TestConfig.java
+++ webServiceSpringBootJPA/src/main/java/com/andrey/webServiceSpringBootJPA/config/TestConfig.java
@@ -39,6 +39,24 @@ public class TestConfig implements CommandLineRunner{
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers"); 
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
 		User u1 = new User(null, "Maria Brown","maria@gmail.com","9888888888","12328372837");
 		User u2 =  new User(null, "Alex Grenn", "alex@gmail.com","1298298912","20912092");
@@ -47,20 +65,10 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
 		
-		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
-		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
-		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
-		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
-
-		
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 	}
 	
