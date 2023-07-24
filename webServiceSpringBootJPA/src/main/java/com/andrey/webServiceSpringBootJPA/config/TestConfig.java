cae333c2aec10d950e84df3443fbd8bf3ff8da05
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-07-24 14:28:10 -0300

    payment e associação implementadas

--- webServiceSpringBootJPA/src/main/java/com/andrey/webServiceSpringBootJPA/config/TestConfig.java
+++ webServiceSpringBootJPA/src/main/java/com/andrey/webServiceSpringBootJPA/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.andrey.webServiceSpringBootJPA.entities.Category;
 import com.andrey.webServiceSpringBootJPA.entities.Order;
 import com.andrey.webServiceSpringBootJPA.entities.OrderItem;
+import com.andrey.webServiceSpringBootJPA.entities.Payment;
 import com.andrey.webServiceSpringBootJPA.entities.Product;
 import com.andrey.webServiceSpringBootJPA.entities.User;
 import com.andrey.webServiceSpringBootJPA.entities.enums.OrderStatus;
@@ -80,7 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"),o1);
 		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
