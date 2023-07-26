c384f517263720578c4550abae65e3b58b7e0cea
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-07-26 14:02:18 -0300

    Tratamento de excecao update

--- webServiceSpringBootJPA/src/main/java/com/andrey/webServiceSpringBootJPA/services/UserService.java
+++ webServiceSpringBootJPA/src/main/java/com/andrey/webServiceSpringBootJPA/services/UserService.java
@@ -13,6 +13,8 @@ import com.andrey.webServiceSpringBootJPA.repositories.UserRepository;
 import com.andrey.webServiceSpringBootJPA.services.exceptions.DatabaseException;
 import com.andrey.webServiceSpringBootJPA.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity =  repository.getReferenceById(id);
-		updateDate(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity =  repository.getReferenceById(id);
+			updateDate(entity, obj);
+			return repository.save(entity);	
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateDate(User entity, User obj) {
