3036e0eb21dd8caeafd366ebb3c1d1120d876391
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-07-26 22:14:09 -0300

    Projeto para Faculdade

---
+++ FaculdadeProgramacaoWeb/src/main/java/com/faculdade/FaculdadeProgramacaoWebApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaculdadeProgramacaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaculdadeProgramacaoWebApplication.class, args);
+	}
+
+}
---
+++ FaculdadeProgramacaoWeb/src/test/java/com/faculdade/FaculdadeProgramacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaculdadeProgramacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
